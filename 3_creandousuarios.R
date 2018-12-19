53628d6424b8696d046ab45f867b129e66803728
Miguel Orjuela <ma.orjuela73@gmail.com>
2018-12-19 10:09:29 -0500

    mod

--- 3_creandousuarios.R
+++ 3_creandousuarios.R
@@ -2,21 +2,9 @@ library(mongolite)
 
 mongourl <- "mongodb://<USER>:<PASSWORD>@test..."
 
-subjects <- mongo("subjects", url = mongourl)
-subjects$insert('{"name" : "blanca"}')
-subjects$find(query = '{}', fields = '{}')
+personas_con <- mongo("personas_", url = mongourl)
+personas_con$insert('{"name" : "blanca"}')
+personas_con$find(query = '{}', fields = '{}')
 str <- c('{"name" : "jerry"}' , '{"name": "anna", "age" : 23}', '{"name": "joe"}')
-subjects$insert(str)
-subjects$find(query = '{}', fields = '{}')
-
-subjects$insert('{"name": "ewdin",
-                  "item": "canvas", 
-                  "qty": "100", 
-                  "tags": ["cotton"], 
-                  "size": {
-                    "h": "28", 
-                    "w": "35.5", 
-                    "uom": "cm"
-                    } 
-                  }')
-subjects$find(query = '{}', fields = '{}')
+personas_con$insert(str)
+personas_con$find(query = '{}', fields = '{}')
